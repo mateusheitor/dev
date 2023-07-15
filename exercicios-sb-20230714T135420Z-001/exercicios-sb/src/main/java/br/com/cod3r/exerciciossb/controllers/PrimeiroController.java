6d42883f1ac789aac4ed2484112306619b10b584
Heitor <127308394+mateusheitor@users.noreply.github.com>
2023-07-14 22:55:32 -0300

    Add files via upload

---
+++ exercicios-sb-20230714T135420Z-001/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,19 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/ola")
+	
+	//utilizando mais de um caminho 
+	//@GetMapping(path = {"/ola", "/saudacao"})
+	
+	public String ola() {
+		return "Ola Spring Boot!!";
+	}
+}
